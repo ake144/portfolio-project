fe01508763edaecdd870ff0d0ac2c255e7bf1b0e
ake144 <tamiratake@gmail.com>
2024-05-16 08:21:31 +0300

    testimonials

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -13,8 +13,27 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 
-interface SlideProps {}
+interface SlideProps {
+title:string;
+description:string;
+author:string;
+location:string;
+}
 
+const slidesContent= [
+    {
+      title: "He is the future",
+      description: "He is the epitome of potential, embodying innovation and determination. With his visionary mindset and unwavering commitment, he's destined to shape the future. A trailblazer in every sense, he's paving the way for what's to come.",
+      author: "Mengistu Abshiro",
+      location: "from USA"
+    },
+    {
+      title: "An Exceptional Programmer",
+      description: "Aklilu is an exceptional programmer and web developer. His coding skills are top-notch, and he always delivers high-quality work on time. Whether it's building a complex web application or solving a tricky bug",
+      author: "Amanuel Garomsa",
+      location: "CS at AAU"
+    }
+  ];
 const Testimonials: React.FC<SlideProps> = () => {
   useEffect(() => {
     const keenSlider = new KeenSlider('#keen-slider', {
@@ -127,46 +146,39 @@ const Testimonials: React.FC<SlideProps> = () => {
                                 </blockquote>
                             </div>
 
-                            {[...Array(2)].map((_, slideIndex) => (
-                                <div key={slideIndex} className="keen-slider__slide">
-                                    <blockquote className="flex h-full flex-col justify-between  p-6 shadow-sm sm:p-8 lg:p-12">
-                                        <div>
-                                            <div className="flex gap-0.5 text-green-500">
-                                                {[...Array(5)].map((_, index) => (
-                                                    <svg
-                                                        key={index}
-                                                        className="h-5 w-5"
-                                                        fill="currentColor"
-                                                        viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                    >
-                                                        <path
-                                                            d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                                                        />
-                                                    </svg>
-                                                ))}
-                                            </div>
+                            {slidesContent.map((slide, slideIndex) => (
+      <div key={slideIndex} className="keen-slider__slide">
+        <blockquote className="flex h-full flex-col justify-between p-6 shadow-sm sm:p-8 lg:p-12">
+          <div>
+            <div className="flex gap-0.5 text-green-500">
+              {[...Array(5)].map((_, index) => (
+                <svg
+                  key={index}
+                  className="h-5 w-5"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
+                  />
+                </svg>
+              ))}
+            </div>
 
-                                            <div className="mt-4">
-                                                <p className="text-2xl font-bold text-rose-600 sm:text-3xl">He is the future</p>
+            <div className="mt-4">
+              <p className="text-2xl font-bold text-rose-600 sm:text-3xl">{slide.title}</p>
+              <p className="mt-4 leading-relaxed dark:text-white text-gray-700">{slide.description}</p>
+            </div>
+          </div>
 
-                                                <p className="mt-4 leading-relaxed dark:text-white text-gray-700">
-                                                He is the epitome of potential, embodying innovation and determination.
-                                                 With his visionary mindset and unwavering commitment, he's destined to shape the future.
-                                                 A trailblazer in every sense, he's paving the way for what's to come.
-                                                </p>
-                                            </div>
-                                        </div>
-
-                                        <footer className="mt-4 text-sm font-medium dark:text-white text-gray-700 sm:mt-6">
-                                            &mdash; Mengistu Abshiro <span className='text-green-500 text-xl'>- </span> from USA
-                                        </footer>
-                                    </blockquote>
-                                </div>
-                            ))}
+          <footer className="mt-4 text-sm font-medium dark:text-white text-gray-700 sm:mt-6">
+            &mdash; {slide.author} <span className="text-green-500 text-xl">- </span> {slide.location}
+          </footer>
+        </blockquote>
+      </div>
+    ))}
                            
-                           
-
                         </div>
 
                         <div className="mt-8 lg:hidden">
