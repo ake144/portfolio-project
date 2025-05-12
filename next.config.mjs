390d77bcb10b94457618e5e256b7766a49b378cc
ake144 <tamiratgedi1@gmail.com>
2025-05-12 11:20:19 +0300

    config file updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,16 @@
 const nextConfig = {
 
     images: {
-        domains: ['images.unsplash.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+
+
     },
 };
 
